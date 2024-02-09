00b0b10e7bfbfd2f600a74817ce714cbaa266464
moon <cogus2010@naver.com>
2024-02-09 21:50:06 +0900

    2024-2-9 뷰 리졸버

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 @Component("/springmvc/old-controller") //스프링빈 이름으로 핸들러를 찾음.
-public class oldController implements Controller {
+public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("oldController.handleRequest");
-        return null;
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
     }
 }
